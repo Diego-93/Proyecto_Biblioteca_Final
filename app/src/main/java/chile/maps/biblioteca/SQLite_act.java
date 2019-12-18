0788628610e6ebcfb95e240a124baf812d8b8f41
Diego-93 <57983921+Diego-93@users.noreply.github.com>
2019-12-18 03:45:37 -0400

    se modifica tablas queda ook

--- app/src/main/java/chile/maps/biblioteca/SQLite_act.java
+++ app/src/main/java/chile/maps/biblioteca/SQLite_act.java
@@ -3,8 +3,6 @@ package chile.maps.biblioteca;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-
 
 import androidx.annotation.Nullable;
 
