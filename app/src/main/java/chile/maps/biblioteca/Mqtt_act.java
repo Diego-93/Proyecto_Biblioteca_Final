ed5fd36e7b1d3f60a4a7b67e479066e2952d4b01
Diego-93 <57983921+Diego-93@users.noreply.github.com>
2019-12-20 18:49:05 -0400

    se corrige conexion

--- app/src/main/java/chile/maps/biblioteca/Mqtt_act.java
+++ app/src/main/java/chile/maps/biblioteca/Mqtt_act.java
@@ -6,13 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import org.eclipse.paho.android.service.MqttAndroidClient;
-import org.eclipse.paho.client.mqttv3.IMqttActionListener;
-import org.eclipse.paho.client.mqttv3.IMqttToken;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-
 import chile.maps.biblioteca.R;
 
 public class Mqtt_act extends AppCompatActivity {
@@ -21,9 +14,7 @@ public class Mqtt_act extends AppCompatActivity {
     static String PASSWORD="GHW1LDR_2019";
     String topicStr="mensaje";
     MqttAndroidClient client;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected <MqttException> void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mqtt_act);
 
@@ -32,8 +23,6 @@ public class Mqtt_act extends AppCompatActivity {
         MqttConnectOptions options = new MqttConnectOptions();
         options.setUserName(USERNAME);
         options.setPassword(PASSWORD.toCharArray());
-
-
         try {
             IMqttToken token = client.connect(options);
             token.setActionCallback(new IMqttActionListener() {
@@ -43,19 +32,15 @@ public class Mqtt_act extends AppCompatActivity {
                     Toast.makeText(getBaseContext(), "exito al conectar", Toast.LENGTH_SHORT).show();
 
                 }
-
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
                     Toast.makeText(getBaseContext(), " error al conectar ", Toast.LENGTH_SHORT).show();
-
                 }
             });
         } catch (MqttException e) {
             e.printStackTrace();
         }
-
     }
-
     public void but1(View v){
         String topic = topicStr;
         String mensaje = "El Principito";
@@ -66,7 +51,6 @@ public class Mqtt_act extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-
     public void but2(View v){
         String topic = topicStr;
         String mensaje = "El Diablo III";
