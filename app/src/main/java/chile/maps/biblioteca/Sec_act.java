e3215263e6a6bdd264868640f35acd60f1ae652e
Diego-93 <57983921+Diego-93@users.noreply.github.com>
2019-12-20 20:50:25 -0400

    se configuran parametros de seguridad

--- app/src/main/java/chile/maps/biblioteca/Sec_act.java
+++ app/src/main/java/chile/maps/biblioteca/Sec_act.java
@@ -1,4 +1,30 @@
-package chile.maps.biblioteca;
+package com.app.biblioteca;
 
-public class Sec_act {
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import chile.maps.biblioteca.R;
+
+public class Sec_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sec_act);
+    }
+
+    public void Incidencia1(View v)
+    {
+        Toast toast1 = Toast.makeText((getApplicationContext()),"Incidencia ocurrida", Toast.LENGTH_SHORT);
+        toast1.setGravity(Gravity.CENTER,0,0);
+    }
+    public void Incidencia2(View v)
+    {
+        Toast toast2 = Toast.makeText((getApplicationContext()),"Incidencia ocurrida 2", Toast.LENGTH_SHORT);
+        toast2.setGravity(Gravity.CENTER,0,0);
+    }
 }
